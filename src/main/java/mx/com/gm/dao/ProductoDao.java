0fecf075c5fbca1e214981ecce214f4b6e36ed47
Martin Bordon <martinbordon36@gmail.com>
2020-11-29 17:18:21 -0300

    respaldo

--- src/main/java/mx/com/gm/dao/ProductoDao.java
+++
@@ -1,8 +0,0 @@
-package mx.com.gm.dao;
-
-import mx.com.gm.domain.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoDao extends JpaRepository<Producto, Long>{
-    
-}
